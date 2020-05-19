c53eb2db659cf0cc5875b12a524c1b613a960f0d
Martin Hauskrecht <hauskrechtmartin@gmail.com>
2020-05-19 17:57:28 +0200

    Add unit test for policy selection
    Signed-off-by: Martin Hauskrecht <hauskrechtmartin@gmail.com>

--- plugin/forward/setup_policy_test.go
+++ plugin/forward/setup_policy_test.go
@@ -1,6 +1,7 @@
 package forward
 
 import (
+	"reflect"
 	"strings"
 	"testing"
 
@@ -42,6 +43,42 @@ func TestSetupPolicy(t *testing.T) {
 
 		if !test.shouldErr && f.p.String() != test.expectedPolicy {
 			t.Errorf("Test %d: expected: %s, got: %s", i, test.expectedPolicy, f.p.String())
+		}
+	}
+}
+
+func TestSetupPolicySelection(t *testing.T) {
+	tests := []struct {
+		input         string
+		shouldErr     bool
+		expectedOrder []string
+		expectedErr   string
+	}{
+		{"forward . 1.1.1.1 {\npolicy sequential \n}\n", false, []string{"1.1.1.1:53"}, ""},
+		{"forward . 1.1.1.1 {\npolicy random\n}\n", false, []string{"1.1.1.1:53"}, ""},
+		{"forward . 1.1.1.1 {\npolicy round_robin\n}\n", false, []string{"1.1.1.1:53"}, ""},
+		{"forward . 1.1.1.1 2.2.2.2 3.3.3.3 4.4.4.4 {\npolicy sequential\n}\n", false, []string{"1.1.1.1:53", "2.2.2.2:53", "3.3.3.3:53", "4.4.4.4:53"}, ""},
+		{"forward . 1.1.1.1 2.2.2.2 3.3.3.3 4.4.4.4 {\npolicy round_robin\n}\n", false, []string{"2.2.2.2:53", "1.1.1.1:53", "3.3.3.3:53", "4.4.4.4:53"}, ""},
+	}
+
+	for i, test := range tests {
+		c := caddy.NewTestController("dns", test.input)
+		f, err := parseForward(c)
+
+		orderedProxies := f.List()
+		var orderedProxiesString []string
+		for _, p := range orderedProxies {
+			orderedProxiesString = append(orderedProxiesString, p.addr)
+		}
+
+		if err != nil {
+			if !test.shouldErr {
+				t.Errorf("Test %d: expected no error but found one for input %s, got: %v", i, test.input, err)
+			}
+		}
+
+		if !test.shouldErr && !reflect.DeepEqual(orderedProxiesString, test.expectedOrder) {
+			t.Errorf("Test %d: expected: %v, got: %v", i, test.expectedOrder, orderedProxiesString)
 		}
 	}
 }
